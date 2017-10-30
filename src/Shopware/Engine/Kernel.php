497d3fc0efe4892617d7cecafcebe0e412c5e85c
Lukasz Lewandowski <llewandowski@gpmd.co.uk>
2017-10-29 20:36:18 -0400

    Move backend cache directory to the project root

--- src/Shopware/Engine/Kernel.php
+++ src/Shopware/Engine/Kernel.php
@@ -11,10 +11,22 @@ namespace luklewluk\ShopwareComposerBridge\Shopware\Engine;
 class Kernel extends \Shopware\Kernel
 {
     /**
+     * Override config file path to use the custom one
+     *
      * @return string
      */
     protected function getConfigPath()
     {
         return __DIR__ . '/Configs/Default.php';
+    }
+
+    /**
+     * Override cache directory to use project root
+     *
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return ROOT_PATH . '/var/cache/' . $this->environment . '_' . \Shopware::REVISION;
     }
 }
